220f61565a1d843b9baf0bee99b91ca897dacfa1
davisanderson11 <davis@rsmla.com>
2025-03-04 14:50:05 -0500

    [Change] Check if on land before displaying info

--- js/map-click.js
+++ js/map-click.js
@@ -1,29 +1,33 @@
 // Add a click event to display popup information for climate and resource features.
 map.on("click", (e) => {
+    const onLand = map.queryRenderedFeatures(e.point, { layers: ['land-fill'] });
     const climateFeatures = map.queryRenderedFeatures(e.point, { layers: ['climate-fill'] });
     const soilFeatures = map.queryRenderedFeatures(e.point, { layers: ['data-soil-fill'] });
   
     // Only show a popup if relevant features are clicked
-    if (!climateFeatures.length && !soilFeatures.length) {
-      return;
-    }
-  
+    if (!onLand.length) {
+        if (!climateFeatures.length && !soilFeatures.length) {
+            return;
+        }
+    }  
+
     let popupHTML = "";
-  
-    if (climateFeatures.length > 0) {
-      const climateFeature = climateFeatures[0];
-      const koppenCode = climateFeature.properties.CODE;
-      popupHTML += `<strong>Koppen Code:</strong> ${koppenCode}<br>`;
+    
+    if (onLand.length > 0) {
+        if (climateFeatures.length > 0) {
+            const climateFeature = climateFeatures[0];
+            const koppenCode = climateFeature.properties.CODE;
+            popupHTML += `<strong>Koppen Code:</strong> ${koppenCode}<br>`;
+          }
+        
+          if (soilFeatures.length > 0) {
+            const soilType = soilFeatures[0].properties.quality || "Unknown Soil Type";
+            popupHTML += `<strong>Soil Class:</strong> ${soilType}`;
+          }
+          new mapboxgl.Popup()
+          .setLngLat(e.lngLat)
+          .setHTML(popupHTML)
+          .addTo(map);
     }
-  
-    if (soilFeatures.length > 0) {
-      const soilType = soilFeatures[0].properties.quality || "Unknown Soil Type";
-      popupHTML += `<strong>Soil Class:</strong> ${soilType}`;
-    }
-  
-    new mapboxgl.Popup()
-      .setLngLat(e.lngLat)
-      .setHTML(popupHTML)
-      .addTo(map);
   });
   
